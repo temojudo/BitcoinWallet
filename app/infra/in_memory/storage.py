e026d4f976b1f0e525d2ea462a46b5109e45f094
stsertsvadze <stser18@freeuni.edu.ge>
2022-02-14 00:24:22 +0400

    changed storage key types

--- app/infra/in_memory/storage.py
+++ app/infra/in_memory/storage.py
@@ -9,5 +9,5 @@ from app.core.wallet.wallet import Wallet
 @dataclass
 class Storage:
     users: Dict[str, User] = field(default_factory=dict)
-    wallets: Dict[str, List[Wallet]] = field(default_factory=dict)
-    transactions: Dict[str, Transaction] = field(default_factory=dict)
+    wallets: Dict[int, List[Wallet]] = field(default_factory=dict)
+    transactions: Dict[int, Transaction] = field(default_factory=dict)
